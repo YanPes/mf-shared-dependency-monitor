547766a1c5c2bbffc6e1d56366449a782f94a05d
YanPes <yannik.peschke@gmail.com>
2025-07-08 16:46:45 +0200

    create dependency accordion and badge component

---
+++ scripts/components/badge-component.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * @param type {"In Sync" | "Out of Sync"}
+ * @constructor
+ */
+
+function BadgeComponent(type) {
+  const badge = document.createElement('div');
+
+  badge.innerText = type;
+
+  badge.classList.add('badge');
+
+  if(type === "In Sync") {
+    badge.classList.add('badge-sync');
+  }
+
+  if(type === "Out of Sync") {
+    badge.classList.add('badge-out-of-sync');
+  }
+
+  return badge;
+}
+
+export default BadgeComponent
---
+++ scripts/components/dependency-accordion.js
@@ -0,0 +1,40 @@
+import BadgeComponent from "./badge-component.js";
+
+/**
+ *
+ * @param name {string}
+ * @param content
+ * @returns {HTMLDivElement}
+ * @constructor
+ */
+
+function DependencyAccordion(name, content) {
+  let isInSync = false;
+  let isExpanded = false;
+
+  const dependencyAccordionDomNode = document.createElement('div');
+  const headerDomNode = document.createElement('div');
+  const nameDomNode = document.createElement('h3');
+  const badgeDomNode = BadgeComponent(isInSync ? "In Sync" : "Out of Sync");
+  const contentDomNode = document.createElement('div');
+
+  nameDomNode.innerText = name;
+
+  headerDomNode.appendChild(nameDomNode);
+  headerDomNode.appendChild(badgeDomNode);
+  dependencyAccordionDomNode.appendChild(headerDomNode);
+  dependencyAccordionDomNode.appendChild(contentDomNode);
+
+  dependencyAccordionDomNode.classList.add(`dependency-accordion`);
+  headerDomNode.classList.add("dependency-accordion-header");
+
+  if (isInSync) {
+    dependencyAccordionDomNode.classList.add("dependency-accordion-in-sync");
+  } else {
+    dependencyAccordionDomNode.classList.add("dependency-accordion-out-of-sync");
+  }
+
+  return dependencyAccordionDomNode;
+}
+
+export default DependencyAccordion;
